17408a77a0fab89db0f147116e73f7201815795e
Eduardo Gurgel <edgurgel@gmail.com>
2014-01-02 23:38:00 -0300

    Update JSEX and add meck to mix.lock

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Pusher.Mixfile do
   defp deps(:dev) do
     [
      {:httpoison, github: "edgurgel/httpoison"},
-     {:jsex, github: "talentdeficit/jsex"},
+     {:jsex, github: "talentdeficit/jsex", ref: "c9df36f07b2089a73ab6b32074c01728f1e5a2e1"},
      {:erlsha2, github: "vinoski/erlsha2" }
     ]
    end
