d4692069ee3df562990cddf453dc4ab85739d9a6
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-12 23:31:44 +1200

    Ensure support to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pusher.Mixfile do
   def project do
     [ app: :pusher,
       version: "0.0.1",
-      elixir: "~> 0.15.0",
+      elixir: "~> 0.15.0 or ~> 1.0.0",
       deps: deps ]
   end
 
