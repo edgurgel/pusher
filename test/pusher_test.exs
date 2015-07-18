378be4ca402ea354ae097a84b3967af9b433f4ed
steve.brazier <meadsteve@gmail.com>
2015-07-18 10:06:02 +0100

    extract out app id setting from each test

--- test/pusher_test.exs
+++ test/pusher_test.exs
@@ -3,13 +3,19 @@ defmodule PusherTest do
   import Mock
   import Pusher
 
+  # `setup` is called before each test is run
+  setup do
+    :application.set_env(:pusher, :app_id, "app_id")
+    {:ok, []}
+  end
+
   test "configure! should change application env" do
-    configure!("host", "port", "app_id", "app_key", "secret")
+    configure!("host", "port", "updated_app_id", "app_key", "secret")
     vars = :application.get_all_env(:pusher)
 
     assert vars[:host]    == "host"
     assert vars[:port]    == "port"
-    assert vars[:app_id]  == "app_id"
+    assert vars[:app_id]  == "updated_app_id"
     assert vars[:app_key] == "app_key"
     assert vars[:secret]  == "secret"
   end
@@ -27,7 +33,6 @@ defmodule PusherTest do
 
   test_with_mock ".trigger sends the payload to a single channel", Pusher.HttpClient,
   [post!: fn("/apps/app_id/events", @expected_payload, _) -> @response_succesful_message end] do
-    :application.set_env(:pusher, :app_id, "app_id")
     result = Pusher.trigger("event-name", "data", "channel")
     expected = 200
     assert result == expected
@@ -40,12 +45,10 @@ defmodule PusherTest do
 
   test_with_mock ".channels calls the http client for list of channels", Pusher.HttpClient,
   [get!: fn("/apps/app_id/channels") -> @response_with_channel end] do
-    :application.set_env(:pusher, :app_id, "app_id")
     result = Pusher.channels
     expected = {200, %{"channels" => %{"test_channel" => %{}}}}
     assert result == expected
   end
-
 
   @response_with_channel_info %HTTPoison.Response{
     body: %{"occupied" => true, "user_count" => 42},
@@ -54,7 +57,6 @@ defmodule PusherTest do
 
   test_with_mock ".channel with an argument returns the user count for the channel", Pusher.HttpClient,
   [get!: fn("/apps/app_id/channels/test_info_channel", %{}, qs: %{info: "subscription_count"}) -> @response_with_channel_info end] do
-    :application.set_env(:pusher, :app_id, "app_id")
     result = Pusher.channel "test_info_channel"
     expected = {200, %{"occupied" => true, "user_count" => 42}}
     assert result == expected
@@ -67,7 +69,6 @@ defmodule PusherTest do
 
   test_with_mock ".users returns users in a presence channel", Pusher.HttpClient,
   [get!: fn("/apps/app_id/channels/presence-foobar/users") -> @response_with_users end] do
-    :application.set_env(:pusher, :app_id, "app_id")
     result = Pusher.users "presence-foobar"
     expected = {200, %{"users" => [%{"id" => 3}, %{"id" => 57}]}}
     assert result == expected
