64e2408a1207b9d0d4080541b23a9aa781075365
Eduardo Gurgel <eduardo@gurgel.me>
2016-02-22 22:39:14 +1300

    Bump version to 0.1.3
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Pusher.Mixfile do
 
   def project do
     [ app: :pusher,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.0",
       name: "Pusher",
       description: @description,
