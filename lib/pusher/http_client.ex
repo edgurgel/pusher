f8ecfb21458253971a2e5538da6558c404e2c182
Eduardo Gurgel <eduardo@gurgel.me>
2019-07-07 14:05:15 +1200

    Refactor everything

--- lib/pusher/http_client.ex
+++ lib/pusher/http_client.ex
@@ -1,38 +1,23 @@
 defmodule Pusher.HttpClient do
   use HTTPoison.Base
   alias Pusher.RequestSigner
+  alias Pusher.Client
 
-  defp process_url(url), do: base_url() <> url
-
-  defp base_url do
-    {:ok, host} = :application.get_env(:pusher, :host)
-    {:ok, port} = :application.get_env(:pusher, :port)
-    "#{host}:#{port}"
-  end
-
-  defp process_response_body(body) do
-    unless body == "", do: body |> JSX.decode!(), else: nil
+  def process_response_body(body) do
+    unless body == "", do: body |> Jason.decode!(), else: nil
   end
 
   @doc """
   More info at: http://pusher.com/docs/rest_api#authentication
   """
   def request(method, path, body \\ "", headers \\ [], options \\ []) do
+    client = Keyword.get(options, :client, %Client{})
+
     query_string =
       Keyword.get(options, :qs, %{})
-      |> RequestSigner.sign_query_string(body, app_key(), secret(), method, path)
+      |> RequestSigner.sign_query_string(body, client.app_key, client.secret, method, path)
       |> URI.encode_query()
 
-    super(method, path <> "?" <> query_string, body, headers, options)
-  end
-
-  defp secret do
-    {:ok, secret} = :application.get_env(:pusher, :secret)
-    secret
-  end
-
-  defp app_key do
-    {:ok, app_key} = :application.get_env(:pusher, :app_key)
-    app_key
+    super(method, "#{client.endpoint}#{path}?#{query_string}", body, headers, options)
   end
 end
--- test/pusher_test.exs
+++ test/pusher_test.exs
@@ -1,59 +1,67 @@
 defmodule PusherTest do
-  use ExUnit.Case
-  import Mock
-  import Pusher
+  use ExUnit.Case, async: true
+  use Mimic
+  alias Pusher.{Client, HttpClient}
 
-  # `setup` is called before each test is run
   setup do
-    :application.set_env(:pusher, :app_id, "app_id")
-    {:ok, []}
+    stub(HttpClient)
+    :ok
   end
 
-  test "configure! should change application env" do
-    configure!("host", "port", "updated_app_id", "app_key", "secret")
-    vars = :application.get_all_env(:pusher)
+  @client %Client{app_id: "le_app_id"}
 
-    assert vars[:host] == "host"
-    assert vars[:port] == "port"
-    assert vars[:app_id] == "updated_app_id"
-    assert vars[:app_key] == "app_key"
-    assert vars[:secret] == "secret"
-  end
-
-  @response_succesful_message %HTTPoison.Response{
+  @response_succesful %HTTPoison.Response{
     body: %{},
     status_code: 200
   }
 
-  @expected_payload %{
-                      name: "event-name",
-                      channels: ["channel"],
-                      data: "data"
-                    }
-                    |> JSX.encode!()
+  @payload %{name: "event-name", channels: ["channel"], data: "data"} |> Jason.encode!()
 
-  test_with_mock ".trigger sends the payload to a single channel", Pusher.HttpClient,
-    post!: fn "/apps/app_id/events", @expected_payload, _ -> @response_succesful_message end do
-    result = Pusher.trigger("event-name", "data", "channel")
-    expected = :ok
-    assert result == expected
-  end
+  @payload_with_socket_id %{
+                            name: "event-name",
+                            channels: ["channel"],
+                            data: "data",
+                            socket_id: "blah"
+                          }
+                          |> Jason.encode!()
 
-  @expected_payload_with_socket_id %{
-                                     name: "event-name",
-                                     channels: ["channel"],
-                                     data: "data",
-                                     socket_id: "blah"
-                                   }
-                                   |> JSX.encode!()
+  describe "trigger/4" do
+    test "successful request" do
+      expect(HttpClient, :post, fn "/apps/le_app_id/events", @payload, _, [client: @client] ->
+        {:ok, @response_succesful}
+      end)
 
-  test_with_mock ".trigger sends the payload with a socket_id", Pusher.HttpClient,
-    post!: fn "/apps/app_id/events", @expected_payload_with_socket_id, _ ->
-      @response_succesful_message
-    end do
-    result = Pusher.trigger("event-name", "data", "channel", "blah")
-    expected = :ok
-    assert result == expected
+      assert Pusher.trigger(@client, "event-name", "data", "channel") == :ok
+    end
+
+    test "unsuccessful request" do
+      response = %HTTPoison.Response{status_code: 400}
+
+      expect(HttpClient, :post, fn "/apps/le_app_id/events", @payload, _, [client: @client] ->
+        {:ok, response}
+      end)
+
+      assert Pusher.trigger(@client, "event-name", "data", "channel") == {:error, response}
+    end
+
+    test "request error" do
+      expect(HttpClient, :post, fn "/apps/le_app_id/events", @payload, _, [client: @client] ->
+        {:error, :reason}
+      end)
+
+      assert Pusher.trigger(@client, "event-name", "data", "channel") == {:error, :reason}
+    end
+
+    test "sends the payload with a socket_id" do
+      expect(HttpClient, :post, fn "/apps/le_app_id/events",
+                                   @payload_with_socket_id,
+                                   _,
+                                   [client: @client] ->
+        {:ok, @response_succesful}
+      end)
+
+      assert Pusher.trigger(@client, "event-name", "data", "channel", "blah") == :ok
+    end
   end
 
   @response_with_channel %HTTPoison.Response{
@@ -61,11 +69,32 @@ defmodule PusherTest do
     status_code: 200
   }
 
-  test_with_mock ".channels calls the http client for list of channels", Pusher.HttpClient,
-    get!: fn "/apps/app_id/channels" -> @response_with_channel end do
-    result = Pusher.channels()
-    expected = {:ok, %{"test_channel" => %{}}}
-    assert result == expected
+  describe "channels/1" do
+    test "successful request" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels", [], [client: @client] ->
+        {:ok, @response_with_channel}
+      end)
+
+      assert Pusher.channels(@client) == {:ok, %{"test_channel" => %{}}}
+    end
+
+    test "unsuccessful request" do
+      response = %HTTPoison.Response{status_code: 400}
+
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels", [], [client: @client] ->
+        {:ok, response}
+      end)
+
+      assert Pusher.channels(@client) == {:error, response}
+    end
+
+    test "request error" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels", [], [client: @client] ->
+        {:error, :reason}
+      end)
+
+      assert Pusher.channels(@client) == {:error, :reason}
+    end
   end
 
   @response_with_channel_info %HTTPoison.Response{
@@ -73,16 +102,42 @@ defmodule PusherTest do
     status_code: 200
   }
 
-  test_with_mock ".channel with an argument returns the user count for the channel",
-                 Pusher.HttpClient,
-                 get!: fn "/apps/app_id/channels/test_info_channel",
-                          %{},
-                          qs: %{info: "subscription_count"} ->
-                   @response_with_channel_info
-                 end do
-    result = Pusher.channel("test_info_channel")
-    expected = {:ok, %{"occupied" => true, "user_count" => 42}}
-    assert result == expected
+  describe "channel/2" do
+    test "channel with an argument returns the user count for the channel" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/test_info_channel",
+                                  %{},
+                                  qs: %{info: "subscription_count"},
+                                  client: @client ->
+        {:ok, @response_with_channel_info}
+      end)
+
+      assert Pusher.channel(@client, "test_info_channel") ==
+               {:ok, %{"occupied" => true, "user_count" => 42}}
+    end
+
+    test "unsuccessful request" do
+      response = %HTTPoison.Response{status_code: 400}
+
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/test_info_channel",
+                                  %{},
+                                  qs: %{info: "subscription_count"},
+                                  client: @client ->
+        {:ok, response}
+      end)
+
+      assert Pusher.channel(@client, "test_info_channel") == {:error, response}
+    end
+
+    test "request error" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/test_info_channel",
+                                  %{},
+                                  qs: %{info: "subscription_count"},
+                                  client: @client ->
+        {:error, :reason}
+      end)
+
+      assert Pusher.channel(@client, "test_info_channel") == {:error, :reason}
+    end
   end
 
   @response_with_users %HTTPoison.Response{
@@ -90,10 +145,37 @@ defmodule PusherTest do
     status_code: 200
   }
 
-  test_with_mock ".users returns users in a presence channel", Pusher.HttpClient,
-    get!: fn "/apps/app_id/channels/presence-foobar/users" -> @response_with_users end do
-    result = Pusher.users("presence-foobar")
-    expected = {:ok, [%{"id" => 3}, %{"id" => 57}]}
-    assert result == expected
+  describe "users/2" do
+    test "returns users in a presence channel" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/presence-foobar/users",
+                                  [],
+                                  client: @client ->
+        {:ok, @response_with_users}
+      end)
+
+      assert Pusher.users(@client, "presence-foobar") == {:ok, [%{"id" => 3}, %{"id" => 57}]}
+    end
+
+    test "unsuccessful request" do
+      response = %HTTPoison.Response{status_code: 400}
+
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/presence-foobar/users",
+                                  [],
+                                  client: @client ->
+        {:ok, response}
+      end)
+
+      assert Pusher.users(@client, "presence-foobar") == {:error, response}
+    end
+
+    test "request error" do
+      expect(HttpClient, :get, fn "/apps/le_app_id/channels/presence-foobar/users",
+                                  [],
+                                  client: @client ->
+        {:error, :reason}
+      end)
+
+      assert Pusher.users(@client, "presence-foobar") == {:error, :reason}
+    end
   end
 end
